e6cafa5be79d99f4458729385a7f9d2cde27c2e0
Hari2babloo <hari2babloo@live.com>
2025-04-01 19:04:25 +0530

    dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -15,7 +15,7 @@ function Dashboard() {
 
     const fetchItems = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/auctions");
+        const res = await axios.get("http://localhost:5001/auctions");
         setItems(res.data);
       } catch (error) {
         console.error("Error fetching auctions:", error);
